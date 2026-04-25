5828d22d513182448e17f8db37a834d9d73516f2
Habib Isiaka <olamilekabisiaka@gmail.com>
2026-04-25 18:51:18 -0300

    Update time analysis.sql

--- time analysis.sql
+++ time analysis.sql
@@ -10,3 +10,18 @@ FROM orders
 WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC(MONTH, order_date)
 ORDER BY order_month;
+
+
+/* =========================================
+   Monthly Revenue & Order Volume
+   Description: Tracks revenue and order count per month
+========================================= */
+
+SELECT
+    DATETRUNC(MONTH, order_date) AS order_month,
+    SUM(sales_amount) AS total_revenue,
+    COUNT(DISTINCT order_id) AS total_orders
+FROM orders
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH, order_date)
+ORDER BY order_month;
